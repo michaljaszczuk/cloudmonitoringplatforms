e57f8527f4ca2f4f7e0a801d542e27bcff7016bd
Michał Jaszczuk <michaljaszczuk@gmail.com>
2025-08-25 18:20:37 +0200

    update content.config.json

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,24 @@
 import { z, defineCollection } from "astro:content";
 import { glob } from 'astro/loaders';
-import { createDirectoryCollection } from "@lib/loaders";
 
-const directory = createDirectoryCollection();
+// Define the schema for the directory collection
+const directory = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{json}', base: "./src/data/directory" }),
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    // Define the image field as an object
+    image: z.object({
+      url: z.string(),
+      width: z.number().optional(),
+      height: z.number().optional()
+    }).optional(),
+    featured: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
+    description: z.string().optional(),
+    link: z.string().optional(),
+  }),
+});
 
 const pages = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/pages" }),
